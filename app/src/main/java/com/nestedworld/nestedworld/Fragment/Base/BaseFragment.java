3c5a6b13de1df7ceccc8bf2367339e63bee23e21
kassisdion <florian.faisant@gmail.com>
2015-09-03 22:09:47 +0900

    add baseFragment

---
+++ app/src/main/java/com/nestedworld/nestedworld/Fragment/Base/BaseFragment.java
@@ -0,0 +1,41 @@
+package com.nestedworld.nestedworld.Fragment.Base;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.ButterKnife;
+
+public abstract class BaseFragment extends Fragment {
+
+    protected String TAG = getClass().getSimpleName();
+
+    protected abstract int getLayoutResource();
+
+    protected abstract void initVariable(Bundle savedInstanceState);
+
+    protected abstract void initData(Bundle savedInstanceState);
+
+    public String toString() {
+        return TAG;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(getLayoutResource(), container, false);
+        ButterKnife.bind(this, rootView);
+        initVariable(savedInstanceState);
+        initData(savedInstanceState);
+        return rootView;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
+    }
+}
