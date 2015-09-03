3588a3146a25e1c3a455189b2ec7ae36fd44edec
kassisdion <florian.faisant@gmail.com>
2015-09-03 22:13:10 +0900

    add login fragment

---
+++ app/src/main/java/com/nestedworld/nestedworld/Fragment/Launch/LoginFragment.java
@@ -0,0 +1,36 @@
+package com.nestedworld.nestedworld.Fragment.Launch;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+
+import com.nestedworld.nestedworld.Fragment.Base.BaseFragment;
+import com.nestedworld.nestedworld.R;
+
+public class LoginFragment extends BaseFragment {
+    @Override
+    protected int getLayoutResource() {
+        return R.layout.fragment_login;
+    }
+
+    @Override
+    protected void initVariable(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    protected void initData(Bundle savedInstanceState) {
+
+    }
+
+    /*
+    ** Utils
+     */
+    public static void load(final FragmentManager fragmentManager) {
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        //fragmentTransaction.setCustomAnimations(R.anim.fade_out, R.anim.fade_in);
+        fragmentTransaction.replace(R.id.container, new LoginFragment());
+        fragmentTransaction.addToBackStack("LoginFragment");
+        fragmentTransaction.commit();
+    }
+}
