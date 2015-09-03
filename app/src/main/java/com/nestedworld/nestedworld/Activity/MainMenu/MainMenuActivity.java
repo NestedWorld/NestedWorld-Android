72c2aefbc86c90c23e76939a043bfbde8cc29e83
kassisdion <florian.faisant@gmail.com>
2015-09-03 22:14:53 +0900

    add mainMenu activity

---
+++ app/src/main/java/com/nestedworld/nestedworld/Activity/MainMenu/MainMenuActivity.java
@@ -0,0 +1,23 @@
+package com.nestedworld.nestedworld.Activity.MainMenu;
+
+import android.os.Bundle;
+
+import com.nestedworld.nestedworld.Activity.Base.BaseActivity;
+import com.nestedworld.nestedworld.R;
+
+public class MainMenuActivity extends BaseActivity {
+    @Override
+    protected int getLayoutResource() {
+        return R.layout.activity_empty;
+    }
+
+    @Override
+    protected void initVariable(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    protected void initData(Bundle savedInstanceState) {
+        //TODO charger le fragment
+    }
+}
