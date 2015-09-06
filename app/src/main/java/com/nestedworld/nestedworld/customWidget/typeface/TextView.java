33e5e31cfbc3b716436f2a06f872caa8cfe89f1d
kassisdion <florian.faisant@gmail.com>
2015-09-06 15:57:39 +0900

    solved bug of editable textView

--- app/src/main/java/com/nestedworld/nestedworld/customWidget/typeface/TextView.java
+++ app/src/main/java/com/nestedworld/nestedworld/customWidget/typeface/TextView.java
@@ -17,7 +17,7 @@ public class TextView extends com.rey.material.widget.TextView {
     }
 
     public TextView(final Context context, final AttributeSet attrs) {
-        this(context, attrs, android.R.attr.editTextStyle);
+        this(context, attrs, android.R.attr.textViewStyle);
     }
 
     public TextView(final Context context, final AttributeSet attrs, final int defStyle) {
