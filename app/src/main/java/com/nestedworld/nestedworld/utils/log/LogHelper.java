3fa9fddc56f763c5a0ceb22d0a2d879639602c29
kassisdion <florian.faisant@gmail.com>
2015-09-25 23:48:39 +0900

    forgot final

--- app/src/main/java/com/nestedworld/nestedworld/utils/log/LogHelper.java
+++ app/src/main/java/com/nestedworld/nestedworld/utils/log/LogHelper.java
@@ -4,33 +4,33 @@ import com.nestedworld.nestedworld.BuildConfig;
 
 import android.util.Log;
 
-public class LogHelper {
+public final class LogHelper {
     static final boolean IS_ENABLE = BuildConfig.DEBUG;
 
     private LogHelper() {
     }
 
-    public static void v(String tag, String msg) {
+    public static void v(final String tag, final String msg) {
         if (IS_ENABLE)
             Log.v(tag, msg);
     }
 
-    public static void d(String tag, String msg) {
+    public static void d(final String tag, final String msg) {
         if (IS_ENABLE)
             Log.d(tag, msg);
     }
 
-    public static void i(String tag, String msg) {
+    public static void i(final String tag, final String msg) {
         if (IS_ENABLE)
             Log.i(tag, msg);
     }
 
-    public static void w(String tag, String msg) {
+    public static void w(final String tag, final String msg) {
         if (IS_ENABLE)
             Log.w(tag, msg);
     }
 
-    public static void e(String tag, String msg) {
+    public static void e(final String tag, final String msg) {
         if (IS_ENABLE)
             Log.e(tag, msg);
     }
