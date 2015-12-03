5374d2a941da1fd9c3a3cddd196eed980d2ba026
kassisdion <florian.faisant@gmail.com>
2015-12-03 10:43:46 +0900

    add googleMap support

--- app/src/main/java/com/nestedworld/nestedworld/fragment/mainMenu/tabs/MapFragment.java
+++ app/src/main/java/com/nestedworld/nestedworld/fragment/mainMenu/tabs/MapFragment.java
@@ -5,8 +5,18 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.nestedworld.nestedworld.R;
 import com.nestedworld.nestedworld.fragment.base.BaseFragment;
+
+import butterknife.Bind;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -14,6 +24,9 @@ import com.nestedworld.nestedworld.fragment.base.BaseFragment;
 public class MapFragment extends BaseFragment {
 
     public final static String FRAGMENT_NAME = MapFragment.class.getSimpleName();
+
+    @Bind(R.id.mapView)
+    MapView mMapView;
 
     public static void load(@NonNull final FragmentManager fragmentManager) {
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
@@ -32,11 +45,55 @@ public class MapFragment extends BaseFragment {
 
     @Override
     protected void initUI(Bundle savedInstanceState) {
+        mMapView.onCreate(savedInstanceState);
+        mMapView.onResume();//We display the map immediately
 
+        try {
+            MapsInitializer.initialize(getActivity().getApplicationContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        GoogleMap googleMap = mMapView.getMap();
+
+        // create marker
+        MarkerOptions marker = new MarkerOptions().position(new LatLng(-34, 151)).title("Hello Maps");
+
+        // Changing marker icon
+        marker.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ROSE));
+
+        // adding marker
+        googleMap.addMarker(marker);
+        CameraPosition cameraPosition = new CameraPosition.Builder().target(new LatLng(-34, 151)).zoom(12).build();
+        googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
     }
 
     @Override
     protected void initLogic(Bundle savedInstanceState) {
 
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mMapView.onLowMemory();
     }
 }
