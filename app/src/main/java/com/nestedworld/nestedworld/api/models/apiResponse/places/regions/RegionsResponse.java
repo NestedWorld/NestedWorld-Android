93013036a4cc51f0b32ebeba01f0bb56a93174ed
florian <florian.faisant@gmail.com>
2016-02-25 21:48:45 +0900

    add /regions models

--- app/src/main/java/com/nestedworld/nestedworld/api/models/apiResponse/places/regions/RegionsResponse.java
+++ app/src/main/java/com/nestedworld/nestedworld/api/models/apiResponse/places/regions/RegionsResponse.java
@@ -1,4 +1,19 @@
 package com.nestedworld.nestedworld.api.models.apiResponse.places.regions;
 
+import com.google.gson.annotations.Expose;
+
+import java.util.ArrayList;
+
 public class RegionsResponse {
+
+    @Expose
+    public ArrayList<Region> regions;
+
+    public static class Region {
+        @Expose
+        public String url;
+
+        @Expose
+        public String name;
+    }
 }
