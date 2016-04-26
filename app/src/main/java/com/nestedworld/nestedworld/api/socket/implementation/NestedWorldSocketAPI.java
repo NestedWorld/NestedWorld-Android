36c2cbe7f0c6582c431d1c80cb63933750ad52b3
florian <florian.faisant@gmail.com>
2016-04-27 01:29:02 +0900

    Connect to official API

--- app/src/main/java/com/nestedworld/nestedworld/api/socket/implementation/NestedWorldSocketAPI.java
+++ app/src/main/java/com/nestedworld/nestedworld/api/socket/implementation/NestedWorldSocketAPI.java
@@ -4,9 +4,11 @@ import android.support.annotation.NonNull;
 
 import com.nestedworld.nestedworld.api.socket.listener.ConnectionListener;
 import com.nestedworld.nestedworld.api.socket.listener.SocketListener;
+import com.nestedworld.nestedworld.helper.log.LogHelper;
 
 public class NestedWorldSocketAPI {
 
+    private final String TAG = getClass().getSimpleName();
     private static NestedWorldSocketAPI mSingleton;
     private final SocketManager mSocketManager;
 
@@ -14,17 +16,20 @@ public class NestedWorldSocketAPI {
     ** Constructor
      */
     private NestedWorldSocketAPI(@NonNull final ConnectionListener connectionListener) {
-        mSocketManager = new SocketManager("10.0.2.2", 2009);
-        mSocketManager.setTimeOut(1000);
+        mSocketManager = new SocketManager("eip.kokakiwi.net", 6464);
+        mSocketManager.setTimeOut(10000);
+        LogHelper.d(TAG, "Waiting for a new connection...");
         mSocketManager.addSocketListener(new SocketListener() {
             @Override
             public void onSocketConnected(@NonNull SocketEvent socketEvent) {
+                LogHelper.d(TAG, "Successfully Fully got a connection");
                 mSingleton = NestedWorldSocketAPI.this;
                 connectionListener.OnConnectionReady(mSingleton);
             }
 
             @Override
             public void onSocketDisconnected(@NonNull SocketEvent socketEvent) {
+                LogHelper.e(TAG, "Connection failed");
                 reset();
                 connectionListener.OnConnectionLost();
             }
