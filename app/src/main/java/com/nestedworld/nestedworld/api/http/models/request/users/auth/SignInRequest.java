36bda8a8e9db1cad1082debe12f0ed1368db40a0
florian <florian.faisant@gmail.com>
2016-05-17 00:01:02 +0900

    solve bug of empty body

--- app/src/main/java/com/nestedworld/nestedworld/api/http/models/request/users/auth/SignInRequest.java
+++ app/src/main/java/com/nestedworld/nestedworld/api/http/models/request/users/auth/SignInRequest.java
@@ -1,8 +1,15 @@
 package com.nestedworld.nestedworld.api.http.models.request.users.auth;
 
+import com.google.gson.annotations.Expose;
+
 public class SignInRequest {
+    @Expose
     final String email;
+
+    @Expose
     final String password;
+
+    @Expose
     final String app_token;
 
     public SignInRequest(String email, String password, String app_token) {
