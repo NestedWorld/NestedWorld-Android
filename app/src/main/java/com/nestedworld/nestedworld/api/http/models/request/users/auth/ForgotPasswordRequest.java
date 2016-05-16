7aff3b168480e9b378e3f61e47ed7308e93fd12a
florian <florian.faisant@gmail.com>
2016-05-17 00:02:41 +0900

    prevent bug of empty body

--- app/src/main/java/com/nestedworld/nestedworld/api/http/models/request/users/auth/ForgotPasswordRequest.java
+++ app/src/main/java/com/nestedworld/nestedworld/api/http/models/request/users/auth/ForgotPasswordRequest.java
@@ -1,6 +1,9 @@
 package com.nestedworld.nestedworld.api.http.models.request.users.auth;
 
+import com.google.gson.annotations.Expose;
+
 public class ForgotPasswordRequest {
+    @Expose
     final String email;
 
     public ForgotPasswordRequest(String email) {
--- app/src/main/java/com/nestedworld/nestedworld/api/http/models/request/users/auth/RegisterRequest.java
+++ app/src/main/java/com/nestedworld/nestedworld/api/http/models/request/users/auth/RegisterRequest.java
@@ -1,8 +1,15 @@
 package com.nestedworld.nestedworld.api.http.models.request.users.auth;
 
+import com.google.gson.annotations.Expose;
+
 public class RegisterRequest {
+    @Expose
     final String email;
+
+    @Expose
     final String password;
+
+    @Expose
     final String pseudo;
 
     public RegisterRequest(String email, String password, String pseudo) {
