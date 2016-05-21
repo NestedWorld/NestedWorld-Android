005e71a2871e339a00e6113c36d17c7341b06060
florian <florian.faisant@gmail.com>
2016-05-21 17:14:04 +0900

    Typo

--- app/src/main/java/com/nestedworld/nestedworld/customView/viewpager/WrapContentViewPager.java
+++ app/src/main/java/com/nestedworld/nestedworld/customView/viewpager/WrapContentViewPager.java
@@ -1,4 +1,4 @@
-package com.nestedworld.nestedworld.customView.wrapcontentViewPager;
+package com.nestedworld.nestedworld.customView.viewpager;
 
 import android.content.Context;
 import android.support.v4.view.ViewPager;
