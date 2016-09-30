f2cd1073a7e5e2828c065693b6262378b9eeeeaf
florian <florian.faisant@gmail.com>
2016-09-30 11:24:21 +0200

    improve buildType

---
+++ app/src/main/java/com/nestedworld/nestedworld/network/NetworkConstant.java
@@ -0,0 +1,12 @@
+package com.nestedworld.nestedworld.network;
+
+public final class NetworkConstant {
+    private NetworkConstant() {
+        //Private constructor for avoiding this class to be construct
+    }
+
+    public enum Environement {
+        DEV,
+        PROD
+    }
+}
