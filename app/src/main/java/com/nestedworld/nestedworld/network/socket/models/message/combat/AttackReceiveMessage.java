a35a6352678f231e400b5274f73a7587cc57ff4e
florian <florian.faisant@gmail.com>
2016-10-11 13:55:03 +0200

    parse attackReceiveMessage

--- app/src/main/java/com/nestedworld/nestedworld/network/socket/models/message/combat/AttackReceiveMessage.java
+++ app/src/main/java/com/nestedworld/nestedworld/network/socket/models/message/combat/AttackReceiveMessage.java
@@ -7,6 +7,7 @@ import com.nestedworld.nestedworld.network.socket.implementation.SocketMessageTy
 import com.nestedworld.nestedworld.network.socket.models.message.DefaultMessage;
 
 import org.msgpack.value.Value;
+import org.msgpack.value.ValueFactory;
 
 import java.util.Map;
 
@@ -14,8 +15,8 @@ public class AttackReceiveMessage extends DefaultMessage {
 
     public String type;
     public String id;
-    public int attack;
-    public int combat;
+    public long attack;
+    public long combat;
     public AttackReceiveMessageMonster monster;
     public AttackReceiveMessageTarget target;
 
@@ -31,20 +32,57 @@ public class AttackReceiveMessage extends DefaultMessage {
      */
     @Override
     protected void unSerialise(@NonNull Map<Value, Value> message) {
-
+        this.type = message.get(ValueFactory.newString("type")).asStringValue().asString();
+        this.id = message.get(ValueFactory.newString("id")).asStringValue().asString();
+        this.attack = message.get(ValueFactory.newString("attack")).asIntegerValue().asLong();
+        this.monster = new AttackReceiveMessageMonster(message.get(ValueFactory.newString("monster")).asMapValue().map(), getMessageKind(), null);
+        this.target = new AttackReceiveMessageTarget(message.get(ValueFactory.newString("target")).asMapValue().map(), getMessageKind(), null);
+        this.combat = message.get(ValueFactory.newString("combat")).asIntegerValue().asLong();
     }
 
     /*
     /** Inner class used for parsing
      */
-    public final static class AttackReceiveMessageMonster {
-        public int id;
+    public final static class AttackReceiveMessageMonster extends DefaultMessage{
+        public long id;
         public int hp;
+
+        /*
+        ** Constructor
+         */
+        public AttackReceiveMessageMonster(@NonNull Map<Value, Value> message, @NonNull SocketMessageType.MessageKind messageKind, @Nullable SocketMessageType.MessageKind idKind) {
+            super(message, messageKind, idKind);
+        }
+
+        /*
+        ** Life cycle
+         */
+        @Override
+        protected void unSerialise(@NonNull Map<Value, Value> message) {
+            this.id = message.get(ValueFactory.newString("id")).asIntegerValue().asLong();
+            this.hp = message.get(ValueFactory.newString("id")).asIntegerValue().asInt();
+        }
     }
 
-    public final static class AttackReceiveMessageTarget {
-        public int id;
+    public final static class AttackReceiveMessageTarget  extends DefaultMessage{
+        public long id;
         public int hp;
+
+        /*
+        ** Constructor
+         */
+        public AttackReceiveMessageTarget(@NonNull Map<Value, Value> message, @NonNull SocketMessageType.MessageKind messageKind, @Nullable SocketMessageType.MessageKind idKind) {
+            super(message, messageKind, idKind);
+        }
+
+        /*
+        ** Life cycle
+         */
+        @Override
+        protected void unSerialise(@NonNull Map<Value, Value> message) {
+            this.id = message.get(ValueFactory.newString("id")).asIntegerValue().asLong();
+            this.hp = message.get(ValueFactory.newString("id")).asIntegerValue().asInt();
+        }
     }
 
 }
