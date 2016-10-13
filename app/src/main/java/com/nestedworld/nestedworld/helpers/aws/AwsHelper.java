28ea1823f962b588ae07a098cd37cdf3bee0d8ed
florian <florian.faisant@gmail.com>
2016-10-13 19:10:24 +0200

    create aws helper

---
+++ app/src/main/java/com/nestedworld/nestedworld/helpers/aws/AwsHelper.java
@@ -0,0 +1,29 @@
+package com.nestedworld.nestedworld.helpers.aws;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.support.annotation.NonNull;
+
+import com.amazonaws.auth.CognitoCachingCredentialsProvider;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferUtility;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+
+public final class AwsHelper {
+    private AwsHelper() {
+        //Private constructor for avoiding this class to be construct
+    }
+
+    public static void upload(@NonNull final Context context, @NonNull final Bitmap bitmap) {
+        /*CognitoCachingCredentialsProvider credentialsProvider = new CognitoCachingCredentialsProvider(
+                context.getApplicationContext(),
+                "COGNITO_IDENTITY_POOL",
+                Regions.EU_CENTRAL_1
+        );
+
+        AmazonS3 s3 = new AmazonS3Client(credentialsProvider);
+        TransferUtility transferUtility = new TransferUtility(s3, context);
+        transferUtility.upload()*/
+    }
+}
