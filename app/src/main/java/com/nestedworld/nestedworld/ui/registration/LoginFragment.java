9ed8fef29df807a729407f058e85d8c533fee309
florian <florian.faisant@gmail.com>
2016-10-15 00:59:24 +0200

    work on loginUI

--- app/src/main/java/com/nestedworld/nestedworld/ui/registration/LoginFragment.java
+++ app/src/main/java/com/nestedworld/nestedworld/ui/registration/LoginFragment.java
@@ -3,6 +3,8 @@ package com.nestedworld.nestedworld.ui.registration;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
@@ -17,6 +19,7 @@ import com.nestedworld.nestedworld.network.http.errorHandler.RetrofitErrorHandle
 import com.nestedworld.nestedworld.network.http.implementation.NestedWorldHttpApi;
 import com.nestedworld.nestedworld.network.http.models.response.users.auth.ForgotPasswordResponse;
 import com.nestedworld.nestedworld.network.http.models.response.users.auth.SignInResponse;
+import com.nestedworld.nestedworld.ui.base.BaseAppCompatActivity;
 import com.nestedworld.nestedworld.ui.base.BaseFragment;
 import com.nestedworld.nestedworld.ui.mainMenu.MainMenuActivity;
 import com.rey.material.widget.ProgressView;
@@ -33,9 +36,13 @@ public class LoginFragment extends BaseFragment {
     public final static String FRAGMENT_NAME = LoginFragment.class.getSimpleName();
 
     @BindView(R.id.editText_userEmail)
-    EditText etEmail;
+    TextInputEditText etEmail;
+    @BindView(R.id.textInputLayout_userEmail)
+    TextInputLayout textInputLayoutUserEmail;
     @BindView(R.id.editText_userPassword)
-    EditText etPassword;
+    TextInputEditText etPassword;
+    @BindView(R.id.textInputLayout_userPassword)
+    TextInputLayout textInputLayoutUserPassword;
     @BindView(R.id.progressView)
     ProgressView progressView;
 
@@ -68,24 +75,126 @@ public class LoginFragment extends BaseFragment {
      */
     @OnClick(R.id.nav_back)
     public void back() {
-        getActivity().onBackPressed();
-    }
-
-    @OnClick(R.id.button_login)
-    public void login() {
-
-        progressView.start();
-
-        final String email = etEmail.getText().toString();
-        final String password = etPassword.getText().toString();
-
+        //Check if fragment hasn't been detach
         if (mContext == null) {
             return;
         }
 
+        ((BaseAppCompatActivity) mContext).onBackPressed();
+    }
+
+    @OnClick(R.id.button_login)
+    public void login() {
+        //Check if fragment hasn't been detach
+        if (mContext == null) {
+            return;
+        }
+
+        //Retrieve user input
+        String email = etEmail.getText().toString();
+        String password = etPassword.getText().toString();
+
+        //Check input
+        if (!checkInputForLogin(email, password)) {
+            return;
+        }
+
+        //Send login request
+        sendLoginRequest(email, password);
+    }
+
+    @OnClick(R.id.textView_forgotPassword)
+    public void forgotPassword() {
+        //Check if fragment hasn't been detach
+        if (mContext == null) {
+            return;
+        }
+
+        //Retrieve user input
+        String email = etEmail.getText().toString();
+
+        //Check user input
+        if (!checkInputForForgotPassword(email)) {
+            return;
+        }
+
+        //Send request
+        NestedWorldHttpApi.getInstance().forgotPassword(email).enqueue(new Callback<ForgotPasswordResponse>() {
+            @Override
+            public void onSuccess(Response<ForgotPasswordResponse> response) {
+                //check if fragment hasn't been detach
+                if (mContext == null) {
+                    return;
+                }
+
+                //Warn user an email has been send
+                Toast.makeText(mContext, getString(R.string.registration_msg_passwordSend), Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onError(@NonNull KIND errorKind, @Nullable Response<ForgotPasswordResponse> response) {
+                //check if fragment hasn't been detach
+                if (mContext == null) {
+                    return;
+                }
+
+                //Get error message
+                String errorMessage = RetrofitErrorHandler.getErrorMessage(mContext, errorKind, getString(R.string.error_request_forgotPassword), response);
+
+                //Display error message
+                Toast.makeText(mContext, errorMessage, Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+
+    /*
+    ** Internal method
+     */
+    private boolean checkInputForLogin(@NonNull final String email, @NonNull final String password) {
+        //Check email
+        if (!email.isEmpty()) {
+            textInputLayoutUserEmail.setErrorEnabled(false);
+        } else {
+            textInputLayoutUserEmail.setErrorEnabled(false);
+            textInputLayoutUserEmail.setError("Please enter your email");
+            return false;
+        }
+
+        //Check password
+        if (!password.isEmpty()) {
+            textInputLayoutUserPassword.setErrorEnabled(false);
+        } else {
+            textInputLayoutUserPassword.setErrorEnabled(false);
+            textInputLayoutUserPassword.setError("Please enter yout password");
+            return false;
+        }
+
+        return true;
+    }
+
+    private boolean checkInputForForgotPassword(@NonNull final String email) {
+        if (email.isEmpty()) {
+            textInputLayoutUserEmail.setErrorEnabled(true);
+            textInputLayoutUserEmail.setError("Please enter your email");
+            return false;
+        } else {
+            textInputLayoutUserEmail.setErrorEnabled(false);
+            textInputLayoutUserPassword.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    private void sendLoginRequest(@NonNull final String email, @NonNull final String password) {
         NestedWorldHttpApi.getInstance().signIn(email, password).enqueue(new Callback<SignInResponse>() {
             @Override
             public void onSuccess(Response<SignInResponse> response) {
+                //Check if fragment hasn't been detach
+                if (mContext == null) {
+                    return;
+                }
+
+                //Stop the loading animation
                 progressView.stop();
 
                 //Create a new session
@@ -98,41 +207,18 @@ public class LoginFragment extends BaseFragment {
 
             @Override
             public void onError(@NonNull KIND errorKind, @Nullable Response<SignInResponse> response) {
+                //Check if fragment hasn't been detach
+                if (mContext == null) {
+                    return;
+                }
+
+                //Stop loading animation
                 progressView.stop();
 
-                final String errorMessage = RetrofitErrorHandler.getErrorMessage(mContext, errorKind, getString(R.string.error_request_login), response);
-                Toast.makeText(mContext, errorMessage, Toast.LENGTH_LONG).show();
-            }
-        });
-    }
+                //Get error message
+                String errorMessage = RetrofitErrorHandler.getErrorMessage(mContext, errorKind, getString(R.string.error_request_login), response);
 
-    @OnClick(R.id.textView_forgotPassword)
-    public void forgotPassword() {
-        final String email = etEmail.getText().toString();
-
-        if (mContext == null) {
-            return;
-        }
-
-        NestedWorldHttpApi.getInstance().forgotPassword(email).enqueue(new Callback<ForgotPasswordResponse>() {
-            @Override
-            public void onSuccess(Response<ForgotPasswordResponse> response) {
-                //check if fragment hasn't been detach
-                if (mContext == null) {
-                    return;
-                }
-
-                Toast.makeText(mContext, getString(R.string.registration_msg_passwordSend), Toast.LENGTH_LONG).show();
-            }
-
-            @Override
-            public void onError(@NonNull KIND errorKind, @Nullable Response<ForgotPasswordResponse> response) {
-                //check if fragment hasn't been detach
-                if (mContext == null) {
-                    return;
-                }
-
-                final String errorMessage = RetrofitErrorHandler.getErrorMessage(mContext, errorKind, getString(R.string.error_request_forgotPassword), response);
+                //Display error message
                 Toast.makeText(mContext, errorMessage, Toast.LENGTH_LONG).show();
             }
         });
