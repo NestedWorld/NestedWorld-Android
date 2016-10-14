9c690ee2f3af513542e77aca5b6d6b7f729e63bc
florian <florian.faisant@gmail.com>
2016-10-15 01:42:06 +0200

    comment aws integration

--- app/src/main/java/com/nestedworld/nestedworld/helpers/aws/AwsHelper.java
+++ app/src/main/java/com/nestedworld/nestedworld/helpers/aws/AwsHelper.java
@@ -1,14 +1,7 @@
 package com.nestedworld.nestedworld.helpers.aws;
 
 import android.content.Context;
-import android.graphics.Bitmap;
 import android.support.annotation.NonNull;
-
-import com.amazonaws.auth.CognitoCachingCredentialsProvider;
-import com.amazonaws.mobileconnectors.s3.transferutility.TransferUtility;
-import com.amazonaws.regions.Regions;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3Client;
 
 import java.io.File;
 
@@ -19,15 +12,15 @@ public final class AwsHelper {
 
     public static void upload(@NonNull final Context context, @NonNull final File file) {
         //TODO use good credential
-        CognitoCachingCredentialsProvider credentialsProvider = new CognitoCachingCredentialsProvider(
-                context.getApplicationContext(),
-                "COGNITO_IDENTITY_POOL",
-                Regions.EU_CENTRAL_1
-        );
+        //CognitoCachingCredentialsProvider credentialsProvider = new CognitoCachingCredentialsProvider(
+        //        context.getApplicationContext(),
+        //        "COGNITO_IDENTITY_POOL",
+        //        Regions.EU_CENTRAL_1
+        //);
 
         //TODO check the doc
-        AmazonS3 s3 = new AmazonS3Client(credentialsProvider);
-        TransferUtility transferUtility = new TransferUtility(s3, context);
-        transferUtility.upload(null, "picture", file);
+        //AmazonS3 s3 = new AmazonS3Client(credentialsProvider);
+        //TransferUtility transferUtility = new TransferUtility(s3, context);
+        //transferUtility.upload(null, "picture", file);
     }
 }
