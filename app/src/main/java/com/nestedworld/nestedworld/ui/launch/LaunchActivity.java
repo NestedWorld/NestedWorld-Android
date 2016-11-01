61bd987005ef08dd43d3e6a49e315323be5f2f8f
florian <florian.faisant@gmail.com>
2016-11-01 12:01:30 +0100

    avoid blocking animation

--- app/src/main/java/com/nestedworld/nestedworld/ui/launch/LaunchActivity.java
+++ app/src/main/java/com/nestedworld/nestedworld/ui/launch/LaunchActivity.java
@@ -1,11 +1,13 @@
 package com.nestedworld.nestedworld.ui.launch;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 import com.nestedworld.nestedworld.R;
+import com.nestedworld.nestedworld.helpers.log.LogHelper;
 import com.nestedworld.nestedworld.helpers.session.SessionHelper;
 import com.nestedworld.nestedworld.ui.base.BaseAppCompatActivity;
 import com.nestedworld.nestedworld.ui.mainMenu.MainMenuActivity;
@@ -36,17 +38,25 @@ public class LaunchActivity extends BaseAppCompatActivity {
         //start animation
         imageView.startAnimation(AnimationUtils.loadAnimation(this, R.anim.rotation));
 
-        //choose the correct activity
-        if (!checkForExistingSession()) {
-            //we don't have a session
-            //So we start the registration activity & kill current activity
-            startActivity(WelcomeActivity.class);
-            finish();
-        } else {
-            //We have a session so we start the main activity
-            startActivity(MainMenuActivity.class);
-            finish();
-        }
+        //Run in background (avoid blocking animation)
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //choose the correct activity
+                if (!checkForExistingSession()) {
+                    //we don't have a session
+                    //So we start the registration activity & kill current activity
+                    LogHelper.d(TAG, "Going for WelcomeActivity");
+                    startActivity(WelcomeActivity.class);
+                    finish();
+                } else {
+                    //We have a session so we start the main activity
+                    LogHelper.d(TAG, "Going for MainMenuActivity");
+                    startActivity(MainMenuActivity.class);
+                    finish();
+                }
+            }
+        }).start();
     }
 
     /*
