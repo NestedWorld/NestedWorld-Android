1180f0e99f93aafa1099f5b540923d2f77c5a989
florian <florian.faisant@gmail.com>
2016-11-01 20:46:41 +0100

    fix compilation issue

--- app/src/main/java/com/nestedworld/nestedworld/network/http/implementation/HttpEndPoint.java
+++ app/src/main/java/com/nestedworld/nestedworld/network/http/implementation/HttpEndPoint.java
@@ -4,33 +4,40 @@ import com.nestedworld.nestedworld.BuildConfig;
 import com.nestedworld.nestedworld.network.NetworkConstant;
 
 public final class HttpEndPoint {
-
-    public final static String USER_INVENTORY = USER_INFO + "inventory/";
-    public final static String USER_INVENTORY_ITEM_DETAIL = USER_INVENTORY + "{itemId}/";
-    //Object related endpoint
-    public final static String OBJECT_LIST = "objects/";
-    public final static String OBJECT_DETAIL = "objects/{objectId}";
     private final static String API_VERSION = "/v1/";
+
     private final static String DEV_URL = "http://api-dev.nestedworld.com";
     private final static String PROD_URL = "http://api.nestedworld.com";
+
     private final static String BASE_URL =
             (BuildConfig.ENVIRONMENT == NetworkConstant.Environement.DEV) ? DEV_URL
                     : (BuildConfig.ENVIRONMENT == NetworkConstant.Environement.PROD) ? PROD_URL
                     : DEV_URL;
+
     public final static String BASE_END_POINT = BASE_URL + API_VERSION;
+
     //Monster related endpoint
     private final static String MONSTERS_PREFIX = "monsters";
     public final static String MONSTERS_LIST = MONSTERS_PREFIX;
     public final static String MONSTER_ATTACK = MONSTERS_PREFIX + "/{monsterId}/attacks";
+
     //Place related endpoint
     private final static String PLACES_PREFIX = "places";
     public final static String PLACES_LIST = PLACES_PREFIX;
     public final static String REGIONS_LIST = PLACES_PREFIX + "/regions";
+
     //User related endpoint
     private final static String USER_PREFIX = "users/";
+    public final static String USER_INFO = USER_PREFIX + "me/";
     public final static String USER_FRIENDS = USER_PREFIX + "friends/";
     public final static String USER_MONSTERS = USER_PREFIX + "monsters";
-    public final static String USER_INFO = USER_PREFIX + "me/";
+    public final static String USER_INVENTORY = USER_INFO + "inventory/";
+
+    //Object related endpoint
+    private final static String OBJECT_PREFIX = "objects/";
+    public final static String OBJECT_LIST = OBJECT_PREFIX;
+    public final static String OBJECT_DETAIL = "objects/{objectId}";
+
     //Auth related endpoint
     private final static String AUTH_PREFIX = USER_PREFIX + "auth/";
     public final static String USER_SIGN_IN = AUTH_PREFIX + "login/simple";
