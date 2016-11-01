38b28ddee21b25ca259fa9db8198d18d8de3eb0e
florian <florian.faisant@gmail.com>
2016-11-01 14:15:19 +0100

    refacto dialogAddFriend

---
+++ app/src/main/java/com/nestedworld/nestedworld/dialog/AddFriendDialog.java
@@ -0,0 +1,155 @@
+package com.nestedworld.nestedworld.dialog;
+
+import android.content.DialogInterface;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.avast.android.dialogs.core.BaseDialogFragment;
+import com.nestedworld.nestedworld.R;
+import com.nestedworld.nestedworld.database.updater.FriendsUpdater;
+import com.nestedworld.nestedworld.network.http.callback.NestedWorldHttpCallback;
+import com.nestedworld.nestedworld.network.http.errorHandler.RetrofitErrorHandler;
+import com.nestedworld.nestedworld.network.http.implementation.NestedWorldHttpApi;
+import com.nestedworld.nestedworld.network.http.models.response.friend.AddFriendResponse;
+import com.rey.material.widget.ProgressView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+import retrofit2.Response;
+
+public class AddFriendDialog extends BaseDialogFragment {
+
+    private final static String TAG = AddFriendDialog.class.getSimpleName();
+
+    @BindView(R.id.editText_addFriend)
+    EditText editTextPseudo;
+    @BindView(R.id.progressView)
+    ProgressView progressView;
+    private Unbinder mUnbinder;
+
+    /*
+    ** Public method
+     */
+    public static void show(@NonNull final FragmentManager fragmentManager) {
+        new AddFriendDialog().show(fragmentManager, TAG);
+    }
+
+    /*
+    ** Life cycle
+     */
+    @Override
+    protected Builder build(Builder initialBuilder) {
+        return initialBuilder
+                .setTitle(R.string.tabHome_title_addFriend)
+                .setMessage(R.string.tabHome_title_addFriendDetail)
+                .setPositiveButton(R.string.tabHome_msg_addFriend, new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        sendAddFriendRequest();
+                    }
+                })
+                .setView(getViewContent());
+    }
+
+    @Override
+    public void onDismiss(DialogInterface dialog) {
+        super.onDismiss(dialog);
+        mUnbinder = null;
+    }
+
+    /*
+    ** Internal method
+     */
+    @NonNull
+    private View getViewContent() {
+        //Create the view
+        View view = LayoutInflater.from(getActivity()).inflate(R.layout.inflater_alertdialog_add_friend, null);
+
+        //Retrieve widget
+        mUnbinder = ButterKnife.bind(this, view);
+
+        //Return the newly created view
+        return view;
+    }
+
+    private void setOnLoadingMode(final boolean loadingMode) {
+        if (loadingMode) {
+            progressView.start();
+            progressView.setVisibility(View.VISIBLE);
+            editTextPseudo.setVisibility(View.GONE);
+        } else {
+            progressView.stop();
+            progressView.setVisibility(View.GONE);
+            editTextPseudo.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void sendAddFriendRequest() {
+        //Start loading animation
+        setOnLoadingMode(true);
+
+        //Retrieve input
+        String pseudo = editTextPseudo.getText().toString();
+
+        //Send request
+        NestedWorldHttpApi
+                .getInstance()
+                .addFriend(pseudo)
+                .enqueue(new NestedWorldHttpCallback<AddFriendResponse>() {
+                    @Override
+                    public void onSuccess(@NonNull Response<AddFriendResponse> response) {
+                        //Check if dialog hasn't been dismiss
+                        if (mUnbinder == null) {
+                            return;
+                        }
+
+                        //Stop loading animation
+                        setOnLoadingMode(false);
+
+                        //Handle response
+                        onAddFriendSuccess();
+                    }
+
+                    @Override
+                    public void onError(@NonNull KIND errorKind, @Nullable Response<AddFriendResponse> response) {
+                        //Check if dialog hasn't been dismiss
+                        if (mUnbinder == null) {
+                            return;
+                        }
+
+                        //Stop loading animation
+                        setOnLoadingMode(false);
+
+                        //Handle response
+                        onAddFriendFailure(RetrofitErrorHandler.getErrorMessage(getContext(), errorKind, response));
+                    }
+                });
+    }
+
+    private void onAddFriendSuccess() {
+        //Update friend list (ORM)
+        new FriendsUpdater().start(null);
+
+        //Display message
+        Toast.makeText(getContext(), R.string.tab_home_msg_addFriendSuccess, Toast.LENGTH_LONG).show();
+    }
+
+    private void onAddFriendFailure(@Nullable String cause) {
+        //Initialise a default error message
+        String errorMessage = getString(R.string.tab_home_msg_addFriendFailed);
+
+        //If we have an error from the server, we complete our default error message
+        if (cause != null) {
+            errorMessage += " : " + cause;
+        }
+
+        //Display the message
+        Toast.makeText(getContext(), errorMessage, Toast.LENGTH_LONG).show();
+    }
+}
--- app/src/main/java/com/nestedworld/nestedworld/ui/mainMenu/tabs/home/HomeFriendFragment.java
+++ app/src/main/java/com/nestedworld/nestedworld/ui/mainMenu/tabs/home/HomeFriendFragment.java
@@ -1,25 +1,18 @@
 package com.nestedworld.nestedworld.ui.mainMenu.tabs.home;
 
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AlertDialog;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import com.nestedworld.nestedworld.R;
 import com.nestedworld.nestedworld.adapter.FriendsAdapter;
 import com.nestedworld.nestedworld.database.models.Friend;
-import com.nestedworld.nestedworld.database.updater.FriendsUpdater;
-import com.nestedworld.nestedworld.database.updater.callback.OnEntityUpdated;
+import com.nestedworld.nestedworld.dialog.AddFriendDialog;
+import com.nestedworld.nestedworld.events.http.OnFriendsUpdatedEvent;
 import com.nestedworld.nestedworld.events.socket.generic.OnResultResponseEvent;
-import com.nestedworld.nestedworld.network.http.callback.NestedWorldHttpCallback;
-import com.nestedworld.nestedworld.network.http.errorHandler.RetrofitErrorHandler;
-import com.nestedworld.nestedworld.network.http.implementation.NestedWorldHttpApi;
-import com.nestedworld.nestedworld.network.http.models.response.friend.AddFriendResponse;
 import com.nestedworld.nestedworld.network.socket.implementation.SocketMessageType;
 import com.nestedworld.nestedworld.network.socket.models.message.combat.AskMessage;
 import com.nestedworld.nestedworld.network.socket.models.message.generic.ResultMessage;
@@ -75,6 +68,11 @@ public class HomeFriendFragment extends BaseFragment {
     ** EventBus
      */
     @Subscribe
+    public void onFriendUpdated(OnFriendsUpdatedEvent onFriendsUpdatedEvent) {
+        populateFriendList();
+    }
+
+    @Subscribe
     public void onResultMessage(OnResultResponseEvent resultResponseEvent) {
         //Check if fragment hasn't been detach
         if (mContext == null) {
@@ -103,7 +101,6 @@ public class HomeFriendFragment extends BaseFragment {
         }
     }
 
-
     /*
     ** Private method
      */
@@ -116,32 +113,6 @@ public class HomeFriendFragment extends BaseFragment {
         //init adapter for our listView
         mAdapter = new FriendsAdapter(mContext);
         listView.setAdapter(mAdapter);
-    }
-
-    private void updateFriendList() {
-        //Check if framgnent hasn't been detach
-        if (mContext == null) {
-            return;
-        }
-
-        //Start loading animation
-        progressView.start();
-
-        new FriendsUpdater().start(new OnEntityUpdated() {
-            @Override
-            public void onSuccess() {
-                populateFriendList();
-
-                //Stop loading animation
-                progressView.stop();
-            }
-
-            @Override
-            public void onError(@NonNull KIND errorKind) {
-                //Stop loading animation
-                progressView.stop();
-            }
-        });
     }
 
     private void populateFriendList() {
@@ -163,70 +134,6 @@ public class HomeFriendFragment extends BaseFragment {
             return;
         }
 
-        //Instantiate a popup
-        AlertDialog.Builder alertDialog = new AlertDialog.Builder(mContext);
-        alertDialog.setTitle(getString(R.string.tabHome_title_addFriend));
-        alertDialog.setMessage(getString(R.string.tabHome_msg_friendPseudo));
-
-        //Inflate popup
-        final View view = getActivity().getLayoutInflater().inflate(R.layout.inflater_alertdialog_add_friend, null);
-        final EditText editTextPseudo = (EditText) view.findViewById(R.id.editText_addFriend);
-        alertDialog.setView(view);
-
-        //Set callback
-        alertDialog.setPositiveButton(getString(R.string.tabHome_msg_addFriend), new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                //Start loading animation
-                progressView.start();
-
-                //Retrieve input
-                String pseudo = editTextPseudo.getText().toString();
-
-                //Send request
-                NestedWorldHttpApi.getInstance().addFriend(pseudo).enqueue(new NestedWorldHttpCallback<AddFriendResponse>() {
-                    @Override
-                    public void onSuccess(@NonNull Response<AddFriendResponse> response) {
-                        //Update Orm and adapter
-                        updateFriendList();
-
-                        //Display message
-                        Toast.makeText(mContext, R.string.tab_home_msg_addFriendSuccess, Toast.LENGTH_LONG).show();
-
-                        //Stop loading animation
-                        progressView.stop();
-                    }
-
-                    @Override
-                    public void onError(@NonNull KIND errorKind, @Nullable Response<AddFriendResponse> response) {
-                        //Initialise a default error message
-                        String errorMessage = getString(R.string.tab_home_msg_addFriendFailed);
-
-                        //Try to retrieve response.body.message
-                        String serverError = RetrofitErrorHandler.getErrorMessage(mContext, errorKind, response);
-
-                        //If we have a serverError we complete our default error message
-                        if (serverError != null) {
-                            errorMessage += " : " + serverError;
-                        }
-
-                        //Display the message
-                        Toast.makeText(mContext, errorMessage, Toast.LENGTH_LONG).show();
-
-                        //Stop loading animation
-                        progressView.stop();
-                    }
-                });
-            }
-        });
-        alertDialog.setNegativeButton(getString(R.string.tabHome_msg_cancel), new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                dialogInterface.cancel();
-            }
-        });
-
-        //Display popup
-        alertDialog.show();
+        AddFriendDialog.show(getChildFragmentManager());
     }
 }
