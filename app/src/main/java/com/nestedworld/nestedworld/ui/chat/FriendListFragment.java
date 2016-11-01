7589a155539cac376f0c751eaed317406af1c5de
florian <florian.faisant@gmail.com>
2016-11-01 13:19:41 +0100

    remove duplicate adapter

--- app/src/main/java/com/nestedworld/nestedworld/ui/chat/FriendListFragment.java
+++ app/src/main/java/com/nestedworld/nestedworld/ui/chat/FriendListFragment.java
@@ -24,6 +24,7 @@ import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.nestedworld.nestedworld.R;
+import com.nestedworld.nestedworld.adapter.FriendsAdapter;
 import com.nestedworld.nestedworld.database.models.Friend;
 import com.nestedworld.nestedworld.database.models.User;
 import com.nestedworld.nestedworld.database.updater.FriendsUpdater;
@@ -162,81 +163,5 @@ public class FriendListFragment extends BaseFragment implements SwipeRefreshLayo
         //Update adapter
         mAdapter.clear();
         mAdapter.addAll(friends);
-    }
-
-    /**
-     * * Custom adapter for displaying friend on the listView
-     **/
-    private static class FriendsAdapter extends ArrayAdapter<Friend> {
-
-        private static final int resource = R.layout.item_friend_home;
-
-        /*
-        ** Constructor
-         */
-        public FriendsAdapter(@NonNull final Context context) {
-            super(context, 0);
-        }
-
-        /*
-        ** Life cycle
-         */
-        @NonNull
-        @Override
-        public View getView(int position, View convertView, @NonNull ViewGroup parent) {
-
-            View view;
-            FriendHolder friendHolder;
-
-            if (convertView == null) {
-                LayoutInflater layoutInflater = ((BaseAppCompatActivity) getContext()).getLayoutInflater();
-                view = layoutInflater.inflate(resource, parent, false);
-
-                friendHolder = new FriendHolder();
-                friendHolder.friendPicture = (ImageView) view.findViewById(R.id.imageView_item_friend);
-                friendHolder.friendName = (TextView) view.findViewById(R.id.textView_item_friend);
-
-                view.setTag(friendHolder);
-            } else {
-                friendHolder = (FriendHolder) convertView.getTag();
-                view = convertView;
-            }
-
-            //get the currentFriend
-            Friend currentFriend = getItem(position);
-            if (currentFriend == null) {
-                return view;
-            }
-
-            //get the currentFriend information
-            User currentFriendInfo = currentFriend.info();
-            if (currentFriendInfo == null) {
-                return view;
-            }
-
-            //display the friend name
-            friendHolder.friendName.setText(currentFriendInfo.pseudo);
-
-            //display a rounded placeHolder for friend's avatar
-            Resources resources = getContext().getResources();
-            Bitmap bitmap = BitmapFactory.decodeResource(resources, R.drawable.default_avatar);
-            RoundedBitmapDrawable roundedBitmapDrawable = RoundedBitmapDrawableFactory.create(resources, bitmap);
-            roundedBitmapDrawable.setCornerRadius(Math.max(bitmap.getWidth(), bitmap.getHeight()) / 2.0f);
-
-            //display friend's avatar
-            Glide.with(getContext())
-                    .load(currentFriendInfo.avatar)
-                    .placeholder(roundedBitmapDrawable)
-                    .centerCrop()
-                    .bitmapTransform(new CropCircleTransformation(getContext()))
-                    .into(friendHolder.friendPicture);
-
-            return view;
-        }
-
-        private static class FriendHolder {
-            public ImageView friendPicture;
-            public TextView friendName;
-        }
     }
 }
