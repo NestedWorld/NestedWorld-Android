59414d960117f916be09d20dd08a1195493f7f78
florian <florian.faisant@gmail.com>
2016-11-11 19:26:39 +0100

    fix zombi entity

--- app/src/main/java/com/nestedworld/nestedworld/database/updater/FriendsUpdater.java
+++ app/src/main/java/com/nestedworld/nestedworld/database/updater/FriendsUpdater.java
@@ -3,11 +3,15 @@ package com.nestedworld.nestedworld.database.updater;
 import android.support.annotation.NonNull;
 
 import com.nestedworld.nestedworld.database.models.Friend;
+import com.nestedworld.nestedworld.database.models.User;
 import com.nestedworld.nestedworld.database.updater.base.EntityUpdater;
 import com.nestedworld.nestedworld.events.http.OnFriendsUpdatedEvent;
 import com.nestedworld.nestedworld.network.http.models.response.users.friend.FriendsResponse;
+import com.orm.query.Select;
 
 import org.greenrobot.eventbus.EventBus;
+
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Response;
@@ -26,7 +30,14 @@ public class FriendsUpdater extends EntityUpdater<FriendsResponse> {
     @Override
     public void updateEntity(@NonNull final Response<FriendsResponse> response) {
         //Delete old entity
-        Friend.deleteAll(Friend.class);
+        List<Friend> oldFriend = Select.from(Friend.class).list();
+        for (Friend friend : oldFriend) {
+            User friendInfo = friend.info();
+            if (friendInfo != null) {
+                friendInfo.delete();
+            }
+            friend.delete();
+        }
 
         //Update foreign key
         for (Friend friend : response.body().friends) {
