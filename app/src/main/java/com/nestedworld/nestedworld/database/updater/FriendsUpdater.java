a812099c48472c390f9a594d119f9ea0ec584584
florian <florian.faisant@gmail.com>
2016-11-26 23:43:34 +0100

    prevent crash

--- app/src/main/java/com/nestedworld/nestedworld/database/updater/FriendsUpdater.java
+++ app/src/main/java/com/nestedworld/nestedworld/database/updater/FriendsUpdater.java
@@ -5,6 +5,7 @@ import android.support.annotation.NonNull;
 import com.nestedworld.nestedworld.database.models.DaoSession;
 import com.nestedworld.nestedworld.database.models.Friend;
 import com.nestedworld.nestedworld.database.models.FriendDao;
+import com.nestedworld.nestedworld.database.models.Player;
 import com.nestedworld.nestedworld.database.models.PlayerDao;
 import com.nestedworld.nestedworld.database.updater.base.EntityUpdater;
 import com.nestedworld.nestedworld.events.http.OnFriendsUpdatedEvent;
@@ -32,18 +33,24 @@ public class FriendsUpdater extends EntityUpdater<FriendsResponse> {
         FriendDao friendDao = dataBase.getFriendDao();
         PlayerDao playerDao = dataBase.getPlayerDao();
 
-        //Delete old entity
+        //Delete player linked to friend
         for (Friend friend : friendDao.loadAll()) {
-            friend.getPlayer().delete();
-            friend.delete();
+            Player player = friend.getPlayer();
+            if (player != null) {
+                player.delete();
+            }
         }
 
-        //Save entity
+        //Delete old player
+        friendDao.deleteAll();
+
+        //Populate foreign key
         for (Friend friend : response.body().friends) {
             playerDao.insertOrReplace(friend.player);
             friend.setPlayerId(friend.player.playerId);
         }
 
+        //Save new entity
         friendDao.insertInTx(response.body().friends);
 
         //Send event
